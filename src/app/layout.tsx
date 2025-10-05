c8727471bb36de927c3406f7aa28b6c03e5ad824
Chibuikem <chibuikemonwuobi23@gmail.com>
2025-10-05 23:53:03 +0100

    deployment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,9 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${inter.variable} ${notoSans.variable} ${sourceSerif.variable} antialiased`}
